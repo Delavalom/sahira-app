62d76785e36cfd0fdf97cddc390db539eb4ddc18
Delavalom <laarvelo@icloud.com>
2023-04-08 16:17:57 -0400

    background image

--- App.tsx
+++ App.tsx
@@ -1,43 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { colors } from "./config/theme";
+import { BlurView } from "expo-blur";
 
 export default function App() {
   return (
-    <LinearGradient
-      style={styles.container}
-      colors={["#715df2", "#000000"]}
-      start={{ x: 0.25, y: 1.0 }}
-      end={{ x: 1.0, y: 0.0 }}
+    <ImageBackground
+      blurRadius={10}
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        overflow: "hidden",
+        zIndex: 50,
+      }}
+      source={require("./assets/primaryImage.png")}
     >
-      <View style={styles.blurCard}>
-        <Text style={styles.text}>Sahira App</Text>
-      </View>
-    </LinearGradient>
+      <Image source={require("./assets/icon.png")} style={styles.blurContainer}></Image>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
-    color: "#fefefe",
+    color: colors.white,
   },
-  blurCard: {
-    position: "relative",
-    height: 250,
-    width: 250,
-    borderWidth: 1,
-    borderRadius: 10,
+  blurContainer: {
+    position: "absolute",
+    top: 0,
+    left: 120,
+    height: 500,
+    width: 350,
+    borderRadius: 20,
     backgroundColor: "transparent",
-    // shadowOffset: {
-    //   height: 5,
-    //   width: 5
-    // },
-    // shadowColor: "white"
+    overflow: "hidden",
   },
   container: {
     flex: 1,
-    backgroundColor: "#000000",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    backgroundColor: colors.black,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
   },
 });
---
+++ config/theme.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    black: "#000000",
+    white: "#fefefe",
+    purple: "#715df2"
+}
